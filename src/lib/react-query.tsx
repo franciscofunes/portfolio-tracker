95253a481d4f30e9539dbfed077e9c6cbd5d42ff
franciscofunes <ffunes90@gmail.com>
2025-04-14 21:46:19 -0300

    chore(lib): setup axios instance and react-query provider

---
+++ src/lib/react-query.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { QueryClient } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ReactNode } from 'react';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchOnMount: false,
+      retry: 2
+    },
+    mutations: {
+      retry: 0,
+    },
+  },
+});
+
+export function QueryProvider({ children }: { children: ReactNode }) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+    </QueryClientProvider>
+  );
+}
