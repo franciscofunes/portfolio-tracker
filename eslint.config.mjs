84dd2ff91e553edaac19616ffcc6ab33c06d6e9e
franciscofunes <ffunes90@gmail.com>
2025-04-27 19:19:29 -0300

    fix(eslint): ignore prisma generated files during linting:

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,7 +17,8 @@ const eslintConfig = [
       "**/.next/**",
       "**/out/**",
       "**/dist/**",
-      "**/build/**"
+      "**/build/**",
+      "**/src/generated/**"
     ]
   },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
