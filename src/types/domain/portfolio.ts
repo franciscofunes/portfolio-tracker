ef0b00eff3bed07b152b403b4a5f072b5c39a969
franciscofunes <ffunes90@gmail.com>
2025-04-28 00:19:54 -0300

    fix(types): next lint fixing errors

--- src/types/domain/portfolio.ts
+++ src/types/domain/portfolio.ts
@@ -1,7 +1,10 @@
+import { Transaction } from "./transaction";
+
 export interface Portfolio {
   id: string;
   title: string;
   userId: string;
   createdAt: string;
   balance: number;
+  transactions: Transaction[]; 
 }
